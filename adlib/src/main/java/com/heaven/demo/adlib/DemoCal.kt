437c79f88853ebeb606a1c0f2c0c627c7c515969
Le Hai Dang <danglh@tripsoft.io>
2024-10-29 09:53:48 +0700

    dqwdasda

---
+++ adlib/src/main/java/com/heaven/demo/adlib/DemoCal.kt
@@ -0,0 +1,9 @@
+package com.heaven.demo.adlib
+
+open class DemoCal {
+
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+}
---
+++ app/src/main/java/com/heaven/demo/demolibjitpack/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.heaven.demo.demolibjitpack
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.heaven.demo.adlib.DemoCal
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        //
+        val cal = DemoCal()
+        Log.e("MainActivity", "onCreate: result: ${cal.add(5, 6)}")
+    }
+}
